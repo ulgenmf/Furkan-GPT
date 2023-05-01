5a8f556449c6d968f0280673dffdfee5ec5d8c9f
xavier <ulgenmf@gmail.com>
2023-05-01 10:57:23 +0200

    History added

--- src/components/custom-components/HeadLines.jsx
+++ src/components/custom-components/HeadLines.jsx
@@ -13,23 +13,6 @@ export default function HeadLines() {
 	];
 
 	return (
-		<div className="h-full bg-sideBar w-2/12 sm-max:hidden  flex flex-col  text-white">
-			<div
-				id="border"
-				className="borders cursor-not-allowed  hover:bg-gray-700  duration-150  h-10 flex items-center w-11/12 self-center mt-3 outline-2 gap-7"
-			>
-				<AiOutlinePlus /> <p>New chat</p>
-			</div>
-			<div className="text-gray-400 mt-3 ml-2 text-xs">Today</div>
-
-			<div className="mx-auto w-11/12 flex items-center justify-center flex-col gap-5 mt-6 p-2 text-center ">
-				{infos.map((info) => (
-					<div className="flex items-center gap-5 w-full  p-4 cursor-not-allowed hover:rounded-xl duration-150  hover:bg-gray-700  justify-center">
-						<BsChatLeft size={17} className="w-12" />
-						<p className="w-full">{info}</p>
-					</div>
-				))}
-			</div>
-		</div>
+		<div className="h-full bg-sideBar w-2/12 sm-max:hidden  flex flex-col  text-white"></div>
 	);
 }
